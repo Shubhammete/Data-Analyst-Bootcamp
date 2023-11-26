d3c9cdd3e9ca0a8f3fb85123ab40a1b192172865
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2023-11-26 14:42:25 +0530

    Added UI of Bubble Game

--- JavaScript/WeatherAPI/script.js
+++ JavaScript/WeatherAPI/script.js
@@ -1,4 +1,5 @@
-
+require('dotenv').config();
+const apikey= process.env.APIKEY;
 const input = document.getElementById("input-city")
 const submit = document.getElementById("submit")
 const content = document.getElementById("content")
@@ -6,7 +7,7 @@ const content = document.getElementById("content")
 submit.addEventListener("click", async ()=>{
     const value = input.value;
     
-    let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=941d059b337748fe8cc82417232210&q=${value}&aqi=yes`)
+    let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apikey}=${value}&aqi=yes`)
     let data = await response.json()
         content.innerHTML=`<h1>Here's Weather of ${data.location.name}</h1>
                             <p>Weather Condition is ${data.current.condition.text}</p>
