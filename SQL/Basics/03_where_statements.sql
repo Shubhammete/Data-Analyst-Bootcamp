0d4ad2384e5f62639cd4e7a95e5b5ffb6f14dd5e
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2024-02-08 01:08:33 +0530

    where statements

---
+++ SQL/Basics/03_where_statements.sql
@@ -0,0 +1,37 @@
+-- Where statements --> <>, <, >, =, AND, OR, Like, null, not null, in
+
+-- <> means not equal to here show all columns that are not equal to John btw case does not matter it can John or john
+SELECT * FROM employeeinfo WHERE FirstName <> 'John'
+
+-- here < is operator selects age less than 30
+SELECT * FROM employeeinfo WHERE Age < 30
+
+-- here < is operator selects age more than or equal to 30
+SELECT * FROM employeeinfo WHERE Age >= 30
+
+-- here AND checks if both conditions are true or not
+SELECT * FROM employeeinfo WHERE Age < 30 AND Gender = 'Female'
+
+-- here OR checks if one of the conditions are true 
+SELECT * FROM employeeinfo WHERE Age < 30 OR Gender = 'Female'
+
+-- LIKE here is use to select specific string with filters like string format
+-- lastname includes s in any position 
+SELECT * FROM employeeinfo WHERE LastName LIKE '%S'
+
+-- lastname includes s in starting position 
+SELECT * FROM employeeinfo WHERE LastName LIKE 'S%'
+
+-- lastname includes s,a,n in starting position 
+-- here order is important if a is at position of n then it will not work
+SELECT * FROM employeeinfo WHERE LastName LIKE 'S%a%n%'
+
+-- NULL and NOT NULL 
+-- Returns all null fields in first case and not null in second
+SELECT * FROM employeeinfo WHERE LastName IS NULL
+SELECT * FROM employeeinfo WHERE LastName IS NOT NULL
+
+-- Here we are using multiple = statement we can avoid it using IN statement 
+SELECT * FROM employeeinfo WHERE LastName = 'Doe' OR LastName = 'Pax'
+-- instead write this
+SELECT * FROM employeeinfo WHERE LastName IN ('Doe','Pax')
