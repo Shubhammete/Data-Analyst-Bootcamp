a7945c91d4cd1ac2e3b9d940ed4e543fdde5cf5f
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2024-03-07 20:19:56 +0530

    JOINS in SQL

---
+++ SQL/Intermediate/joins.sql
@@ -0,0 +1,26 @@
+-- Inner join which is default join
+-- joins two table common fields present
+SELECT * FROM Employeeinfo
+INNER JOIN EmployeeSalary  -- JOIN EmployeeSalary
+ON Employeeinfo.employeeID = EmployeeSalary.employeeID
+
+-- Full outer join 
+-- shows all rows if not present in another table fills NULL 
+SELECT * FROM Employeeinfo
+Full OUTER JOIN EmployeeSalary
+ON Employeeinfo.employeeID = EmployeeSalary.employeeID
+
+-- Left outer join
+-- shows all the rows and columns present in first table and NUll if not in second table
+
+SELECT * FROM Employeeinfo
+left OUTER JOIN EmployeeSalary
+ON Employeeinfo.employeeID = EmployeeSalary.employeeID
+
+
+-- right outer join
+-- shows all the rows and columns present in second table and NUll if not in first table
+
+SELECT * FROM Employeeinfo
+right OUTER JOIN EmployeeSalary
+ON Employeeinfo.employeeID = EmployeeSalary.employeeID
