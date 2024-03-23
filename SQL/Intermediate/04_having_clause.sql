2d5738fc3c70b23de10c553caa0f8e664448fcd3
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2024-03-23 20:58:20 +0530

    Having clause

--- SQL/Intermediate/04_having_clause.sql
+++ SQL/Intermediate/04_having_clause.sql
@@ -5,4 +5,5 @@ Group by JobTitle
 having count(JobTitle) > 1
 
 -- Here we use having clause to filter according to condition 
--- we cannot use where as it is aggregate columns
+-- we cannot use where as it is aggregate columns and real one from table
+-- we use having below group by as it is dependent on it
